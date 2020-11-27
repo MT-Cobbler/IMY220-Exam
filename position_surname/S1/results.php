be47919315f1bafdd5829d57b1434761cdce664a
MT-Cobbler <mtschoeman123@gmail.com>
2020-11-27 08:18:34 +0200

    added files

---
+++ position_surname/S1/results.php
@@ -0,0 +1,6 @@
+<?php
+    // See all errors and warnings
+    error_reporting(E_ALL);
+
+    // replace with your position_surname
+?>
---
+++ position_surname/S2/script.js
@@ -0,0 +1,3 @@
+// replace this with your position_surname
+
+// Your code goes here
